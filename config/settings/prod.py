780b102e47f838238ae2ae38f75ff528b5c1eb03
Denilson Freitas <contato@freitas-data.com>
2025-03-27 15:45:15 -0300

    feito

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,4 +1,6 @@
 from .base import *
 
 DEBUG = False
-ALLOWED_HOSTS = ['api.freitas-data.com']
+ALLOWED_HOSTS = ['*']  # Você pode depois trocar pelo domínio final do Railway
+
+CSRF_TRUSTED_ORIGINS = ['https://*.railway.app']
